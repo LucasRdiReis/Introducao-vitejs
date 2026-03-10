e7dde23870b2a8455f3fdc216c2abb4e8da4c867
Lucas <lucasredi.reispiva@gmail.com>
2026-03-10 10:54:34 -0300

    aula3

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,9 @@
+function Exemplo01(){
+return(
+    <>
+    <h1>Exemplo 1</h1>
+    <p>componente básico para a renderização da tela com react</p>
+    </>
+)
+}
+export default Exemplo01;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>  
+      <h1>Aula React com Vite JS</h1>
+    </>
+  )
+}
+
+export default Home
